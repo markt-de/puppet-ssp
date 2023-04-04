df66af48de40615aedafccb6bcfb839e878f881e
Stealthking001 <124579618+Stealthking001@users.noreply.github.com>
2023-04-04 11:21:17 +0200

    Update ssp_spec.rb

--- spec/classes/ssp_spec.rb
+++ spec/classes/ssp_spec.rb
@@ -7,7 +7,20 @@ describe 'ssp' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile.with_all_deps }
+      context 'with default parameters' do
+        let(:params) do
+          {
+            version: '1.5.2'
+          }
+        end
+        
+        it { is_expected.to compile }
+        it { is_expected.to contain_class('ssp::config') }
+        it { is_expected.to contain_class('ssp::install') }
+        it { is_expected.to contain_file('/opt/self-service-password-1.5.2/conf/config.inc.local.php') }
+        it { is_expected.to contain_archive('/opt/self-service-password-1.5.2.tar.gz').with_creates('/opt/self-service-password-1.5.2') }
+        it { is_expected.to contain_package('smarty3').only_with_ensure('installed') }
+      end
     end
   end
 end
