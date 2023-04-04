22ccfd88b580c0ec8e27ad732a7519b008a2a22c
Stealthking001 <124579618+Stealthking001@users.noreply.github.com>
2023-04-04 11:38:08 +0200

    Revert "add simple unit tests (#3)" (#4)
    This reverts commit fdbc5184a9811349d16eea9bc0fbc8578c164438.

--- spec/classes/ssp_spec.rb
+++ spec/classes/ssp_spec.rb
@@ -7,20 +7,7 @@ describe 'ssp' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      context 'with default parameters' do
-        let(:params) do
-          {
-            version: '1.5.2'
-          }
-        end
-        
-        it { is_expected.to compile }
-        it { is_expected.to contain_class('ssp::config') }
-        it { is_expected.to contain_class('ssp::install') }
-        it { is_expected.to contain_file('/opt/self-service-password-1.5.2/conf/config.inc.local.php') }
-        it { is_expected.to contain_archive('/opt/self-service-password-1.5.2.tar.gz').with_creates('/opt/self-service-password-1.5.2') }
-        it { is_expected.to contain_package('smarty3').only_with_ensure('installed') }
-      end
+      it { is_expected.to compile.with_all_deps }
     end
   end
 end
